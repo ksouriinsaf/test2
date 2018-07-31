b6428b9f21d854442475ffb0e9809d972433d7e9
yahiaAbou <yahiaaboulhassen@gmail.com>
2018-07-31 19:04:08 +0100

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={	
+'ApiUrl':"https://www.foodysa.com/mobileapp/api",	
+'DialogDefaultTitle':"foody app",	
+'pushNotificationSenderid':"336533240554",	
+'facebookAppId':"", 
+'APIHasKey':"1e15b20c98e007409295607cc806308a"
+};
---
+++ www/js/share.js
@@ -0,0 +1,29 @@
+
+function fbShare(){
+	window.plugins.socialsharing.shareViaFacebook('حمل تطبيق #فوودي الان  واستمتع باسهل طريقه للطلب اونلاين اطلب و #خليها_علينا ', 
+                                                 "https://d.top4top.net/p_931am2dq1.png", 
+                                                 "http://onelink.to/k9c7s6", 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Facebook App not installed on your device, Cannot Share")}
+                                                 );
+};
+ 
+function whatsappShare(){
+	window.plugins.socialsharing.shareViaWhatsApp('حمل تطبيق #فوودي الان  واستمتع باسهل طريقه للطلب اونلاين اطلب و #خليها_علينا ', 
+                                                 "https://d.top4top.net/p_931am2dq1.png", 
+                                                 "http://onelink.to/k9c7s6", 
+                                                 null, 
+                                                 function(errormsg){alert("Error: WhatsApp not installed on your device,Cannot Share")}
+                                                 );
+};
+ 
+function twitterShare(){
+	 window.plugins.socialsharing.shareViaTwitter('حمل تطبيق #فوودي الان  واستمتع باسهل طريقه للطلب اونلاين اطلب و #خليها_علينا ', 
+                                                 "https://d.top4top.net/p_931am2dq1.png", 
+                                                 "http://onelink.to/k9c7s6", 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Twitter App not installed on your device, Cannot Share")}
+                                                 );
+  };
+
+  
